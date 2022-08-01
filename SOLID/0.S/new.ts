d2dce71ec607bb72186ee3de2f5bf726690f69a1
DenGian <mondelaers.ian@gmail.com>
2022-08-01 10:40:40 +0200

    new ts done

--- SOLID/0.S/new.ts
+++ SOLID/0.S/new.ts
@@ -1,26 +1,43 @@
 class Car {
-    //it is convention to start property names in TypeScript with an underscore.
-    // If you want to known why, remove the underscore and see if your compiler is throwing you an error!
-    private _musicLevel : number = 0;
-    private _oldMusicLevel : number = 50;
-    private _fuel : number = 0;
-    private _miles : number = 0;
-    private _engineStatus: boolean = false;
 
-    //By changing this variable to readonly I have in essence created a property constant.
-    // the only subtle difference is that you can write once to the variable inside the constructor
-    private readonly MAXIMUM_FUEL_CAPACITY: number;
-    private readonly FUEL_MILEAGE: number = 10;
+    private readonly _engine: Engine;
+    private readonly _radio: Radio;
 
     constructor(MAXIMUM_FUEL_CAPACITY: number) {
-        this.MAXIMUM_FUEL_CAPACITY = MAXIMUM_FUEL_CAPACITY;
+        this._engine = new Engine(MAXIMUM_FUEL_CAPACITY);
+        this._radio = new Radio();
     }
 
-    get miles(): number {
-        return this._miles;
+
+    public get engine():Engine{
+        return this._engine;
     }
 
-//Take attention to these getters and setters
+
+    public get radio(): Radio{
+        return this._radio
+    }
+
+
+
+    drive() {
+        if(this._engine.Status === false || this._engine.fuel <= 0) {
+            //what I am doing here is a good principle called "failing early"
+            // If you have some conditions you need to check, that will exclude most of the code in your function check that first
+            // This prevents your "happy path" of code to be deeply indented.
+            return;
+        }
+
+        this._engine.consumeFuel();
+        this._engine.addMileage();
+
+    }
+}
+class Radio {
+    private _musicLevel : number = 0;
+    private _oldMusicLevel : number = 50;
+
+    //Take attention to these getters and setters
     get musicLevel(): number {
         return this._musicLevel;
     }
@@ -37,6 +54,27 @@ class Car {
     turnMusicOff() {
         this._musicLevel = 0;
     }
+}
+
+class Engine {
+    private _fuel : number = 0;
+    private _miles : number = 0;
+    private _Status: boolean = false;
+
+    //By changing this variable to readonly I have in essence created a property constant.
+    // the only subtle difference is that you can write once to the variable inside the constructor
+    private readonly MAXIMUM_FUEL_CAPACITY: number;
+    private readonly FUEL_MILEAGE: number = 10;
+
+    constructor(MAXIMUM_FUEL_CAPACITY: number) {
+        this.MAXIMUM_FUEL_CAPACITY = MAXIMUM_FUEL_CAPACITY;
+    }
+
+    get miles(): number {
+        return this._miles;
+    }
+
+
 
     get fuel(): number {
         return this._fuel;
@@ -49,30 +87,29 @@ class Car {
         this._fuel = Math.min(fuel + this._fuel, this.MAXIMUM_FUEL_CAPACITY);
     }
 
-    get engineStatus(): boolean {
-        return this._engineStatus;
+    get Status(): boolean {
+        return this._Status;
     }
 
     turnEngineOn() {
-        this._engineStatus = true;
+        this._Status = true;
     }
 
     turnEngineOff() {
-        this._engineStatus = false;
+        this._Status = false;
     }
 
-    drive() {
-        if(this.engineStatus === false || this._fuel <= 0) {
-            //what I am doing here is a good principle called "failing early"
-            // If you have some conditions you need to check, that will exclude most of the code in your function check that first
-            // This prevents your "happy path" of code to be deeply indented.
-            return;
-        }
 
+    consumeFuel(){
         this._fuel -= 1;
+    }
+
+
+    addMileage(){
         this._miles += this.FUEL_MILEAGE;
     }
 }
+
 
 // When you see <cast>variable this is a "cast" of a variable, explicitly telling the code what the type of this variable will be.
 // This is sometimes needed when a default JS function does not return a precise enough Type.
@@ -89,55 +126,60 @@ const audioElement = <HTMLAudioElement>document.querySelector('#car-music');
 
 let car = new Car(100);
 
+
 musicToggleElement.addEventListener('click', () => {
-    if(car.musicLevel === 0) {
-        car.turnMusicOn();
-        musicSliderElement.value = car.musicLevel.toString();
+    if(car.radio.musicLevel === 0) {
+        car.radio.turnMusicOn();
+        musicSliderElement.value = car.radio.musicLevel.toString();
         musicToggleElement.innerText = 'Turn music off';
         return;
     }
     musicToggleElement.innerText = 'Turn music on';
-    car.turnMusicOff();
+    car.radio.turnMusicOff();
 });
+
 
 //I use input instead of change, because then the value changes when I move the mouse, not only on release
 musicSliderElement.addEventListener('input', (event) => {
     let target = <HTMLFormElement>(event.target);
 
-    car.musicLevel = target.value;
-    audioElement.volume = car.musicLevel / 100;
+    car.radio.musicLevel = target.value;
+    audioElement.volume = car.radio.musicLevel / 100;
 
     //@todo when you are repeating the same text over and over again maybe we should have made some constants for it? Can you do improve on this?
-    musicToggleElement.innerText = car.musicLevel ? 'Turn music off' : 'Turn music on';
+    musicToggleElement.innerText = car.radio.musicLevel ? 'Turn music off' : 'Turn music on';
 });
 
+
 engineToggleElement.addEventListener('click', () => {
-    if(car.engineStatus) {
-        car.turnEngineOff();
+    if(car.engine.Status) {
+        car.engine.turnEngineOff();
         engineToggleElement.innerText = 'Turn engine on';
         return;
     }
     engineToggleElement.innerText = 'Turn engine off';
-    car.turnEngineOn();
+    car.engine.turnEngineOn();
 });
+
 
 addFuelForm.addEventListener('submit', (event) => {
     event.preventDefault();
 
-    car.addFuel(Number(addFuelInput.value));
-    fuelLevelElement.innerText = car.fuel.toString();
+    car.engine.addFuel(Number(addFuelInput.value));
+    fuelLevelElement.innerText = car.engine.fuel.toString();
 });
+
 
 setInterval(() => {
     car.drive();
 
     //while it looks like both lines below are the same there is a subtle difference (you could put breakpoints here to see the difference):
     // this <cast> will only tell TypeScript that the value is a string, but the actual variable in JS is not changed in any way: it is in reality still a number
-    milesElement.innerText = <string><unknown>(car.miles);
+    milesElement.innerText = <string><unknown>(car.engine.miles);
     // This .toString() will actually convert the value in JavaScript from an integer to a string
-    fuelLevelElement.innerText = car.fuel.toString();
+    fuelLevelElement.innerText = car.engine.fuel.toString();
 
-    if(car.musicLevel === 0) {
+    if(car.radio.musicLevel === 0) {
         audioElement.pause();
     } else {
         audioElement.play();
